c6f87ebef1c2f7444afd36781984faebdf32a3af
Responsive Wireframe <heuffjeremy@gmail.com>
2024-11-15 12:26:18 +0100

    Cinemachine + Movement aangepast
    Cinemachine toegevoegd en de movement aangepast zodat je alleen naar voren kan.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
     // Declared alle variables samen onder Vector 3
     Vector3 up = Vector3.zero,
-        right = new Vector3(0, 90, 0),
-        down = new Vector3(0, 180, 0),
-        left = new Vector3(0, 270, 0),
         currentDirection = Vector3.zero;
 
     Vector3 nextPos, destination, direction;
 
-    float speed = 5f;
+    public float speed = 5f;
 
     void Start()
     {
@@ -29,40 +26,15 @@ public class PlayerMovement : MonoBehaviour
 
     void Move()
     {
-        // Rotate when arriving at the destination
-        if (Vector3.Distance(destination, transform.position) <= 0.00001f)
-        {
-            transform.localEulerAngles = currentDirection;
-        }
-
-        // Move the character towards the destination
-        transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
-
-        // Check for movement inputs and update direction and destination
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
         {
             nextPos = Vector3.forward;
             currentDirection = up;
             destination = transform.position + nextPos;
         }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            nextPos = Vector3.back;
-            currentDirection = down;
-            destination = transform.position + nextPos;
-        }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            nextPos = Vector3.right;
-            currentDirection = right;
-            destination = transform.position + nextPos;
-        }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            nextPos = Vector3.left;
-            currentDirection = left;
-            destination = transform.position + nextPos;
-        }
+
+        transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
     }
+
 
 }
